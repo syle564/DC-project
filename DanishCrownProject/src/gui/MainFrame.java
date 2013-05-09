ed404a6311686eaf113aaa39c452c10560346527
Momo <erkoles@abv.bg>
2013-05-09 14:08:46 +0200

    GuI part

--- DanishCrownProject/src/gui/MainFrame.java
+++ DanishCrownProject/src/gui/MainFrame.java
@@ -6,8 +6,22 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
+
+import model.Load;
+import model.LoadingDock;
+import model.Order;
+import model.Status;
+import model.Suborder;
+import model.Trailer;
+import model.Type;
+import service.DU;
+import service.LoadTimeComparator;
+import service.Service;
+import dao.DataBase;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.Comparator;
 
 public class MainFrame {
 	static {
@@ -28,6 +42,27 @@ public class MainFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		Service service=Service.getInstance();
+		LoadingDock loadingD=service.createLoadingDock(1, Type.BOX, Status.OPEN);
+		Order o1=service.createOrder(1, 2000, 10, Type.BOX);
+		Trailer t1=service.createTrailer("1", "Cock", "Douche", "23123", Type.BOX);
+		Suborder s1=service.createSuborder(30, 200, DU.createDate(), o1, t1);
+		Suborder s2=service.createSuborder(25, 200, DU.createDate(), o1, t1);
+		service.registerIn("1", 100, 30);
+		Trailer t2=service.createTrailer("5", "Cock", "Douche", "23123", Type.CHRISTMAS_TREE);
+		Trailer t3=service.createTrailer("2", "Cock", "Douche", "23123", Type.BIN);
+		service.beginLoad(s1.getlLoad());
+		service.beginLoad(s2.getlLoad());
+		for(LoadingDock l:DataBase.getInstance().getAllLoadingDocks() )
+			
+			//System.out.println(l.getlLoad());
+
+		service.completeLoad(s1);
+		//System.out.println(s1.getlLoad());
+		Comparator c=new LoadTimeComparator();
+		
+		loadingD.addLoad(new Load(DU.createDate(), DU.createDate()));
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
